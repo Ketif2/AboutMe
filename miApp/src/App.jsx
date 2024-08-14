be4faa6eb86d18be69e5ecd9b9a06236b70ce494
Ketif2 <kevin.revelo01@epn.edu.ec>
2024-08-14 15:22:42 -0500

    Preparing enviroment

---
+++ miApp/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Presentation from './components/Presentation'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Presentation/>
+      </div>
+    </>
+  )
+}
+
+export default App
